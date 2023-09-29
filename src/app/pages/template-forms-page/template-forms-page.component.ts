e874d584a03589c84ce5ab0c3dc0a522578fb4d6
Dikendev <diken.dev@gmail.com>
2023-09-28 22:53:00 -0300

    Template form initial html and validations

--- src/app/pages/template-forms-page/template-forms-page.component.ts
+++ src/app/pages/template-forms-page/template-forms-page.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserInfo } from 'src/app/model/user-info.interface';
 
 @Component({
   selector: 'app-template-forms-page',
   templateUrl: './template-forms-page.component.html',
-  styleUrls: ['./template-forms-page.component.scss']
+  styleUrls: ['./template-forms-page.component.scss'],
 })
-export class TemplateFormsPageComponent {
+export class TemplateFormsPageComponent implements OnInit, AfterViewInit {
+  public bannedWords = ['admin', 'administrator', 'super_admin', 'moderator'];
 
+  userInfo: UserInfo = {
+    firstName: 'Diego',
+    lastName: 'Guimarães',
+    nickname: '',
+    username: 'diken',
+    email: '',
+    yearOfBirth: 2023,
+    passport: '',
+    fullAddress: '',
+    city: '',
+    postCode: 0,
+    password: 12345,
+    confirmPassword: 0,
+  };
+
+  @ViewChild(NgForm)
+  public formDir!: NgForm;
+
+  public initFormValues: unknown;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    queueMicrotask(() => {
+      this.initFormValues = this.formDir.value;
+    });
+  }
+
+  get years() {
+    const now = new Date().getUTCFullYear();
+    return Array(now - 1949)
+      .fill('')
+      .map((_, index) => now - index);
+  }
+
+  get isAdult() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.userInfo.yearOfBirth >= 18;
+  }
+
+  onSubmit(ngForm: NgForm, event: SubmitEvent) {
+    console.log('ngForm', ngForm);
+    console.log('event', event);
+    console.log('Form submitted!!');
+  }
+
+  onReset(ngForm: NgForm, event: Event) {
+    event.preventDefault();
+    this.formDir.resetForm(this.initFormValues);
+  }
 }
