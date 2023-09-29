759791d075a073867e218e06f572ed45e30b4067
Dikendev <diken.dev@gmail.com>
2023-09-28 22:52:14 -0300

    User interface 🔥

---
+++ src/app/model/user-info.interface.ts
@@ -0,0 +1,14 @@
+export interface UserInfo {
+  firstName: string;
+  lastName: string;
+  nickname: string;
+  username: string;
+  email: string;
+  yearOfBirth: number;
+  passport: string;
+  fullAddress: string;
+  city: string;
+  postCode: number;
+  password: number;
+  confirmPassword: number;
+}
