7a8491a323effd7dd4f2e37950cfc25001f7cbeb
Dikendev <diken.dev@gmail.com>
2023-10-02 19:43:10 -0300

    Routes to reactive form component page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TemplateFormsPageComponent } from './pages/template-forms-page/template-forms-page.component';
+import { ReactiveFormsPageComponent } from './pages/reactive-forms-page/reactive-forms-page/reactive-forms-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'template', pathMatch: 'full' },
-  { path: 'template', component: TemplateFormsPageComponent },
+  {
+    path: 'template',
+    title: 'Template-Driven Forms Playground',
+    component: TemplateFormsPageComponent,
+  },
+  {
+    path: 'reactive-forms',
+    title: 'Reactive Forms Playground',
+    component: ReactiveFormsPageComponent,
+  },
 ];
 
 @NgModule({
