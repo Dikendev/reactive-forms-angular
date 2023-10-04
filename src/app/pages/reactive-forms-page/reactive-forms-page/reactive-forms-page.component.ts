f57a8c3cedba00e28d940e7937bd636954125499
Dikendev <diken.dev@gmail.com>
2023-10-03 21:18:57 -0300

    initial reactive form using formBuilder

--- src/app/pages/reactive-forms-page/reactive-forms-page/reactive-forms-page.component.ts
+++ src/app/pages/reactive-forms-page/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroupDirective, Validators } from '@angular/forms';
+import { banWords } from 'src/app/validators/ban-words.validators';
 
 @Component({
   selector: 'app-reactive-forms-page',
   templateUrl: './reactive-forms-page.component.html',
-  styleUrls: ['./reactive-forms-page.component.scss']
+  styleUrls: ['./reactive-forms-page.component.scss'],
 })
 export class ReactiveFormsPageComponent {
+  @ViewChild(FormGroupDirective)
+  public formDir!: FormGroupDirective;
 
+  public useForm = this._formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    nickName: [''],
+    username: [],
+    email: [''],
+    yearOfBirth: [''],
+    passport: [''],
+    address: [''],
+    phones: [''],
+    skills: [''],
+    password: [''],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
 }
