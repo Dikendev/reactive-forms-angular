f81c5e1feb086fadd744d7659d958c3a6e5cc0a7
Dikendev <diken.dev@gmail.com>
2023-10-05 23:32:05 -0300

    Add user request  service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,40 @@ export class UserService {
     return this._httpclient.get<any[]>(
       `https://jsonplaceholder.typicode.com/users?username=${username}`
     );
+  }
+
+  factories: any = [
+    {
+      id: 2,
+      name: 'Fábrica I',
+      panels: [
+        { id: 11, name: 'M111' },
+        { id: 245, name: '245' },
+      ],
+    },
+    {
+      id: 5,
+      name: 'Fábrica VII',
+      panels: [
+        { id: 120, name: 'M120' },
+        { id: 33, name: 'M33' },
+      ],
+    },
+    {
+      id: 6,
+      name: 'Fábrica II',
+      panels: [
+        { id: 23232, name: 'M23232' },
+        { id: 12323, name: 'M12323' },
+        { id: 123, name: 'M123' },
+      ],
+    },
+  ];
+
+  fetchData(formData: any): Observable<any> {
+    return of({
+      status: 'success',
+      data: this.factories,
+    }).pipe(delay(2000));
   }
 }
