5083173ad81d68ca26c6465673e02bcdf577f449
Dikendev <diken.dev@gmail.com>
2023-10-21 09:39:41 -0300

    add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Login data', loginData);
+      // ... further logic
+    }
+  }
+}
