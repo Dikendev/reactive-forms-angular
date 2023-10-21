a99f6cd012a66b173b60e797c91a3895542ddac0
Dikendev <diken.dev@gmail.com>
2023-10-21 09:49:32 -0300

    map rxjs operator

--- src/app/pages/rxjs-basics-page/rxjs-basics-page.component.ts
+++ src/app/pages/rxjs-basics-page/rxjs-basics-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { fromEvent, map } from 'rxjs';
 
 @Component({
@@ -7,9 +7,16 @@ import { fromEvent, map } from 'rxjs';
   styleUrls: ['./rxjs-basics-page.component.scss'],
 })
 export class RxjsBasicsPageComponent implements OnInit {
+  clicks = fromEvent<PointerEvent>(document, 'click');
+
+  // map this operator applies a projection to each value and emits that projection in the output Observable.
+  positions = this.clicks.pipe(map((event) => event.clientX));
+
+  // emit a constant value not matter the cause.
+  greetings = this.clicks.pipe(map((data) => 'Parabéns'));
+
   ngOnInit(): void {
     this.positions.subscribe((x) => console.log(x));
+    this.greetings.subscribe((x) => console.log(x));
   }
-  clicks = fromEvent<PointerEvent>(document, 'click');
-  positions = this.clicks.pipe(map((event) => event.clientX));
 }
