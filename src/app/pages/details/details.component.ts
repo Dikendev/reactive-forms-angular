c0bd35b40ccd6f4e109201f56e21314812b0fb18
Dikendev <diken.dev@gmail.com>
2023-10-25 23:29:05 -0300

    add details component

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss'],
+})
+export class DetailsComponent {}
